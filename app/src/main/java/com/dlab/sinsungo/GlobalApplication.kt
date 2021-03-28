83db18b4e74e14605a77c78b536dbee398f7bc1e
leesh96 <suho2718@gmail.com>
2021-03-28 13:17:25 +0900

    #18 [feature] : MainActivity 초기화
    바텀네비게이션뷰, 프래그먼트 프레임 레이아웃 초기화
    app 수준 gradle에 databinding 허용

--- app/src/main/java/com/dlab/sinsungo/GlobalApplication.kt
+++ app/src/main/java/com/dlab/sinsungo/GlobalApplication.kt
@@ -1,7 +1,7 @@
 package com.dlab.sinsungo
 
 import android.app.Application
-import com.kakao.sdk.common.KakaoSdk
+// import com.kakao.sdk.common.KakaoSdk
 
 class GlobalApplication : Application() {
     override fun onCreate() {
@@ -9,6 +9,6 @@ class GlobalApplication : Application() {
         // 다른 초기화 코드들
 
         // Kakao SDK 초기화
-        KakaoSdk.init(this, getString(R.string.kakao_app_key))
+        //KakaoSdk.init(this, getString(R.string.kakao_app_key))
     }
 }
