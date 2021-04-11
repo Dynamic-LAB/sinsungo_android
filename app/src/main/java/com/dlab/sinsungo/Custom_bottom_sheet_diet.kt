9595a59debec02e9609567bc643a3279a18c3609
Yg323 <14dnfnfn@gmail.com>
2021-04-11 21:44:19 +0900

    #70 [style] : 식단 커스텀 다이럴로그 view 개발

---
+++ app/src/main/java/com/dlab/sinsungo/Custom_bottom_sheet_diet.kt
@@ -0,0 +1,17 @@
+package com.dlab.sinsungo
+
+import android.app.Dialog
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+
+class Custom_bottom_sheet_diet : BottomSheetDialogFragment() {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        super.onCreateView(inflater, container, savedInstanceState)
+        return inflater.inflate(R.layout.dialog_diet, container, false)
+    }
+}
