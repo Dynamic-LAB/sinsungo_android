5aece0d4699de86229fd6d1ae8568a707aa6d7b0
jaeseuk <sjs4126@naver.com>
2021-05-02 21:03:58 +0900

    #120 [feature] : 레시피 리사이클러뷰 구현
    RecipeAdapter.kt 생성
    CustomBindingAdapter: 이미지 로딩 및 레시피 바인딩 추가
    LoginService.kt 삭제
    AndroidManifest.xml: RecipeDetailActivity 추가
    MainActivity.kt: RecipeFragment로 이동 추가
    LoginRepository.kt 위치 이동
    LoginViewModel.kt import 경로 수정

--- app/src/main/java/com/dlab/sinsungo/CustomBindingAdapter.kt
+++ app/src/main/java/com/dlab/sinsungo/CustomBindingAdapter.kt
@@ -1,9 +1,13 @@
 package com.dlab.sinsungo
 
 import android.view.View
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.databinding.BindingAdapter
 import androidx.databinding.BindingConversion
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.dlab.sinsungo.data.model.Recipe
 
 object CustomBindingAdapter {
     @JvmStatic
@@ -16,5 +20,24 @@ object CustomBindingAdapter {
     @BindingAdapter("exDateFormat")
     fun dateFormatting(view: TextView, value: String) {
         view.text = value.replace("-", ".")
+    }
+
+    @BindingAdapter("imageUrl")
+    @JvmStatic
+    fun loadImage(imageView: ImageView, url: String) {
+        Glide.with(imageView.context).load(url).error(R.drawable.image_empty_refrigerator).thumbnail(0.1f)
+            .into(imageView)
+    }
+
+    @BindingAdapter(*["recipeData", "scrollTop"])
+    @JvmStatic
+    fun bindRecipe(recyclerView: RecyclerView, recipes: ArrayList<Recipe>?, scrollTop: Boolean) {
+        val adapter = recyclerView.adapter as RecipeAdapter
+
+        adapter.submitList(recipes) {
+            if (scrollTop) {
+                recyclerView.scrollToPosition(0)
+            }
+        }
     }
 }
