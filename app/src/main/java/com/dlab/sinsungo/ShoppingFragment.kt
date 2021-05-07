e8eb819e15c6ce0ba3312eb42e9b31f8f2cf866a
Yg323 <14dnfnfn@gmail.com>
2021-05-07 20:31:44 +0900

    #142 [feat] : shopping list recyclerview create

--- app/src/main/java/com/dlab/sinsungo/ShoppingFragment.kt
+++ app/src/main/java/com/dlab/sinsungo/ShoppingFragment.kt
@@ -8,13 +8,13 @@ import android.text.Spannable
 import android.text.SpannableString
 import android.text.TextWatcher
 import android.text.style.ForegroundColorSpan
-import android.util.Log
 import android.view.*
 import android.widget.PopupMenu
 import androidx.annotation.MenuRes
 import androidx.core.content.res.ResourcesCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.dlab.sinsungo.data.model.Shopping
 import com.dlab.sinsungo.databinding.DialogShoppingBinding
 import com.dlab.sinsungo.databinding.FragmentShoppingBinding
@@ -29,11 +29,17 @@ class ShoppingFragment : Fragment(), SpeedDialView.OnActionSelectedListener {
     private lateinit var dialog: AlertDialog
 
     private val viewModel: ShoppingViewModel by viewModels()
+    private val REF_ID = 5
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         binding = FragmentShoppingBinding.inflate(inflater, container, false)
+        binding.lifecycleOwner = this
+        binding.viewModel = viewModel
         binding.sdvShopping.setOnActionSelectedListener(this)
+
         initSpeedDialItem()
+        initRcView()
+
         return binding.root
     }
 
@@ -98,17 +104,9 @@ class ShoppingFragment : Fragment(), SpeedDialView.OnActionSelectedListener {
         setDialogColor(1, R.color.dim_grey)
         setDialogColor(2, R.color.dim_grey)
         initShopping()
-        initViewModel()
         dialog.setCanceledOnTouchOutside(false)
     }
 
-    private fun initViewModel() {
-        dialogView.viewModel = viewModel
-        dialogView.lifecycleOwner = this
-        viewModel.shoppings.observe(viewLifecycleOwner) {
-            Log.d("shopping result", it.toString())
-        }
-    }
 
     private fun initShopping() {
         setTitleSpanColor(Color.parseColor(resources.getString(R.string.color_royal_blue)))
@@ -121,15 +119,16 @@ class ShoppingFragment : Fragment(), SpeedDialView.OnActionSelectedListener {
         dialogView.btnAccept.setOnClickListener {
             val ingredientName = dialogView.etIngredient.text.toString()
             val ingredientCount = dialogView.etCount.text.toString()
-            if ((ingredientName.isEmpty() || ingredientName.isBlank()) && (ingredientCount.isEmpty() || ingredientCount.isBlank())){
+
+            if ((ingredientName.isEmpty() || ingredientName.isBlank()) && (ingredientCount.isEmpty() || ingredientCount.isBlank())) {
                 dialogView.tvInputNoti1.visibility = View.VISIBLE
                 dialogView.tvInputNoti2.visibility = View.VISIBLE
                 setDialogColor(0, R.color.free_speech_red)
                 setDialogColor(1, R.color.free_speech_red)
-            } else if (ingredientName.isEmpty() || ingredientName.isBlank()){
+            } else if (ingredientName.isEmpty() || ingredientName.isBlank()) {
                 dialogView.tvInputNoti1.visibility = View.VISIBLE
                 setDialogColor(0, R.color.free_speech_red)
-            } else if (ingredientCount.isEmpty() || ingredientCount.isBlank()){
+            } else if (ingredientCount.isEmpty() || ingredientCount.isBlank()) {
                 dialogView.tvInputNoti2.visibility = View.VISIBLE
                 setDialogColor(1, R.color.free_speech_red)
             } else {
@@ -139,25 +138,19 @@ class ShoppingFragment : Fragment(), SpeedDialView.OnActionSelectedListener {
                         ingredientCount.toInt(),
                         dialogView.tvCountType.text.toString(),
                         dialogView.etMemo.text.toString(),
-                        5
+                        REF_ID
                     )
                 viewModel.setShopping(newShopping)
                 dialog.dismiss()
             }
-
         }
     }
 
-    private fun setTitleSpanColor(color: Int) {
-        val title = dialogView.tvDialogTitle.text
-        val spannableString = SpannableString(title)
-        spannableString.setSpan(
-            ForegroundColorSpan(color),
-            8,
-            10,
-            Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
-        )
-        dialogView.tvDialogTitle.text = spannableString
+    private fun initRcView() {
+        binding.rcviewShopping.apply {
+            layoutManager = LinearLayoutManager(this.context)
+            adapter = ShoppingListAdapter()
+        }
     }
 
     private fun initPopupMenus() {
@@ -225,12 +218,24 @@ class ShoppingFragment : Fragment(), SpeedDialView.OnActionSelectedListener {
             override fun afterTextChanged(s: Editable?) {
                 val input = s.toString()
                 if (input.isEmpty() || input.isBlank()) {
-                    setDialogColor(2, R.color.free_speech_red)
-                } else {
                     setDialogColor(2, R.color.dim_grey)
+                } else {
+                    setDialogColor(2, R.color.royal_blue)
                 }
             }
         })
+    }
+
+    private fun setTitleSpanColor(color: Int) {
+        val title = dialogView.tvDialogTitle.text
+        val spannableString = SpannableString(title)
+        spannableString.setSpan(
+            ForegroundColorSpan(color),
+            8,
+            10,
+            Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
+        )
+        dialogView.tvDialogTitle.text = spannableString
     }
 
     private fun setDialogColor(choice: Int, color: Int) {
