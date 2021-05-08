215f2fb27b31057d8de6f49143ae675f0412a8ae
Yg323 <14dnfnfn@gmail.com>
2021-05-08 22:13:42 +0900

    #159 [feat] : shopping recyclerview item swipe Helper create

--- app/src/main/java/com/dlab/sinsungo/ShoppingFragment.kt
+++ app/src/main/java/com/dlab/sinsungo/ShoppingFragment.kt
@@ -1,5 +1,6 @@
 package com.dlab.sinsungo
 
+import android.annotation.SuppressLint
 import android.app.AlertDialog
 import android.graphics.Color
 import android.os.Bundle
@@ -8,12 +9,14 @@ import android.text.Spannable
 import android.text.SpannableString
 import android.text.TextWatcher
 import android.text.style.ForegroundColorSpan
+import android.util.TypedValue
 import android.view.*
 import android.widget.PopupMenu
 import androidx.annotation.MenuRes
 import androidx.core.content.res.ResourcesCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.dlab.sinsungo.data.model.Shopping
 import com.dlab.sinsungo.databinding.DialogShoppingBinding
@@ -146,10 +149,22 @@ class ShoppingFragment : Fragment(), SpeedDialView.OnActionSelectedListener {
         }
     }
 
+    @SuppressLint("ClickableViewAccessibility")
     private fun initRcView() {
+        val swipeHelperCallback = SwipeHelperCallback().apply {
+            setClamp(TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 130f, context?.resources?.displayMetrics))
+        }
+        val itemTouchHelper = ItemTouchHelper(swipeHelperCallback)
+        itemTouchHelper.attachToRecyclerView(binding.rcviewShopping)
         binding.rcviewShopping.apply {
             layoutManager = LinearLayoutManager(this.context)
-            adapter = ShoppingListAdapter()
+            adapter = ShoppingListAdapter(viewModel)
+            setHasFixedSize(true)
+            addItemDecoration(ItemDecoration())
+            setOnTouchListener { _, _ ->
+                swipeHelperCallback.removePreviousClamp(this)
+                false
+            }
         }
     }
 
