5a5798758485eaff92e91849ba89f4f2c4427d80
leesh96 <suho2718@gmail.com>
2021-06-01 01:01:38 +0900

    #246 [feat] : 튜토리얼 초대코드 입력 다이얼로그 뷰 구현
    튜토리얼 초대코드 입력 다이얼로그 뷰 구현

--- app/src/main/java/com/dlab/sinsungo/TutorialActivity.kt
+++ app/src/main/java/com/dlab/sinsungo/TutorialActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
 import androidx.viewpager2.adapter.FragmentStateAdapter
@@ -11,7 +12,7 @@ import androidx.viewpager2.widget.ViewPager2
 import com.dlab.sinsungo.databinding.ActivityTutorialBinding
 import com.dlab.sinsungo.viewmodel.TutorialViewModel
 
-class TutorialActivity : FragmentActivity() {
+class TutorialActivity : AppCompatActivity() {
     private lateinit var binding: ActivityTutorialBinding
     private val viewModel: TutorialViewModel by viewModels()
 
